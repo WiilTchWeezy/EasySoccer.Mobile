6ea00a5a2e9680fe58dd745f7f021f6e7d9f553c
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2019-11-19 22:56:53 -0300

    Ajustes

---
+++ EasySoccer.Mobile.API/ApiResponses/SoccerPitchResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace EasySoccer.Mobile.API.ApiResponses
+{
+    public class SoccerPitchResponse
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+    }
+}
--- EasySoccer.Mobile/EasySoccer.Mobile/ViewModels/SoccerPitchSearchViewModel.cs
+++ EasySoccer.Mobile/EasySoccer.Mobile/ViewModels/SoccerPitchSearchViewModel.cs
@@ -1,16 +1,34 @@
-﻿using Prism.Commands;
+﻿using EasySoccer.Mobile.API.ApiResponses;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace EasySoccer.Mobile.ViewModels
 {
 	public class SoccerPitchSearchViewModel : BindableBase
 	{
+        public ObservableCollection<SoccerPitchResponse> SoccerPitchs { get; set; }
         public SoccerPitchSearchViewModel()
         {
+            SoccerPitchs = new ObservableCollection<SoccerPitchResponse>();
+            LoadDataAsync();
+        }
 
+        private void LoadDataAsync()
+        {
+            SoccerPitchs.Add(new SoccerPitchResponse {
+                Image = "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f00000vgXC1EAM/591de07de4b021f908345636.jpg&w=710&h=462",
+                Name = "Spot"
+            });
+
+            SoccerPitchs.Add(new SoccerPitchResponse
+            {
+                Image = "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f00000vgXC1EAM/591de07de4b021f908345636.jpg&w=710&h=462",
+                Name = "All Star"
+            });
         }
 	}
 }
