c1a0aebedd72bb74a17cff47a8bb4949774eb21d
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2020-02-16 13:14:42 -0300

    SplashScreen on android

---
+++ EasySoccer.Mobile/EasySoccer.Mobile.Android/SplashActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EasySoccer.Mobile.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        public override void OnBackPressed() { }
+
+        async void SimulateStartup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
