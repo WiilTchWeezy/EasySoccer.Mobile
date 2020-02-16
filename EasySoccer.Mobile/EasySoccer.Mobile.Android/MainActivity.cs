a2828cc74d995858993b6ccd8a2a7d1a3ca2c299
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2020-02-15 22:51:28 -0300

    Changing Application Name on android

--- EasySoccer.Mobile/EasySoccer.Mobile.Android/MainActivity.cs
+++ EasySoccer.Mobile/EasySoccer.Mobile.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using System;
 
 namespace EasySoccer.Mobile.Droid
 {
-    [Activity(Label = "EasySoccer.Mobile", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "EasySoccer", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
